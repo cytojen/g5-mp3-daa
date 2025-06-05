3595024068e1164f2f9064cef76758c7e1da74b5
Jen Patrick Nataba <150793733+cytojen@users.noreply.github.com>
2025-06-05 19:42:41 +0800

    chore: removed outdated files

--- src/views/rmm.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def show_rmm_page():
-    """RMM page content"""
-    st.title("Russian Multiplication")
-    st.write("This section will contain the RMM algorithm visualization and implementation.")
-    
-    # Placeholder for RMM content
-    st.info("RMM algorithm content will be implemented here.")
