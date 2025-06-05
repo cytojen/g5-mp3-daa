8f9bd14d01e9618f4663bc250ba21c6b99894b5c
Jen Patrick Nataba <150793733+cytojen@users.noreply.github.com>
2025-06-05 19:47:05 +0800

    feat: added russian multiplication page

---
+++ src/views/rmm_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def rmm_page():
+    """RMM page content"""
+    st.title("Russian Multiplication")
+    st.write("This section will contain the RMM algorithm visualization and implementation.")
+    
+    # Placeholder for RMM content
+    st.info("RMM algorithm content will be implemented here.")
