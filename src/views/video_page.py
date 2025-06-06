2769ee56d0c3b534dbd198ec8bed0c18a6c7a7a3
Jen Patrick Nataba <150793733+cytojen@users.noreply.github.com>
2025-06-06 17:41:14 +0800

    feat: added video page tab

---
+++ src/views/video_page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def video_page():
+    """Video documentation page"""
+
+    st.title("Video Documentation")
+    st.write("This page will contain the pre-recorded video documentation and demo of the algorithm.")
+    
+    # Placeholder for video content
+    st.info("Video cards will be inserted here.")
+    
